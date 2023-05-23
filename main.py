a36c1e792ba691747f54ef74dd4dd80cbf69dd27
syusuke9999 <syusukeogino@gmailcom>
2023-05-23 23:34:41 +0900

    Fix typo.

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import json
 import logging
 import datetime
 import pytz
-jst = pytz.timezone('Asia/Tokyo')
 
+jst = pytz.timezone('Asia/Tokyo')
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")

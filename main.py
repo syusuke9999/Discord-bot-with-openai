bc651437bf3d15caf309bb3893b83c9d42844570
syusuke9999 <syusukeogino@gmailcom>
2023-05-25 19:00:22 +0900

    Change system message.

--- main.py
+++ main.py
@@ -21,11 +21,17 @@ jst = pytz.timezone('Asia/Tokyo')
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 DISCORD_TEST_TOKEN = os.getenv("DISCORD_TEST_TOKEN")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-REDISGREEN_URL = os.environ.get('REDISGREEN_URL')
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = os.environ.get('REDIS_PORT')
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
 
 if not debug_mode:
     # Redis接続を初期化
-    r = redis.StrictRedis.from_url(REDISGREEN_URL)
+    r = redis.Redis(
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        password=REDIS_PASSWORD)
+    print("Redis connection established!")
 
 model_name = "gpt-4"
 encoding: Encoding = tiktoken.encoding_for_model(model_name)
@@ -71,15 +77,16 @@ class MyBot(commands.Bot):
             now_of_month = now.strftime("%m")
             now_of_day = now.strftime("%d")
             now_of_time = now.strftime("%H:%M")
-            system_message_content = f"Today is the year {now_of_year}, " \
-                                     f"the month is {now_of_month} and the date {now_of_day}. " \
-                                     f"The current time is {now_of_time}." \
-                                     f"You are a Discord bot joining a Discord channel where people enjoy " \
-                                     f"online games. Have fun talking about Dead by Daylight and Apex legend, " \
-                                     f"and other everyday conversation. with the channel participants." \
-                                     f"Avoid mentioning the topic of the prompt and greet them considering " \
-                                     f"the current time." \
-                                     f"Don't use English, please communicate only in Japanese."
+            system_message_content = f"Today is the year {now_of_year}, the month is {now_of_month} and the date " \
+                                     f"{now_of_day}. The current time is {now_of_time}.You are a Discord bot " \
+                                     f"participating in a Discord channel where people interested in a Discord bot " \
+                                     f"that works with OpenAI's API, are welcome to drop by." \
+                                     f"Have a conversation with the user about " \
+                                     f"how a Discord bot working with OpenAI's API can be useful, " \
+                                     f"without specifying the topic." \
+                                     f"Avoid mentioning the topic of the prompt and greet them " \
+                                     f"considering the current time.Don't use English, " \
+                                     f"please communicate only in Japanese."
             system_message = {"role": "system", "content": system_message_content}
             print(system_message)
             print("user:" + self.user.display_name + "message.content: ", message.content)

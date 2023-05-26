af4838bb4880046edf1742f825157bf4f963133a
syusuke9999 <syusukeogino@gmailcom>
2023-05-27 01:22:14 +0900

    Fix

--- main.py
+++ main.py
@@ -137,8 +137,7 @@ class MyBot(commands.Bot):
             bot_response_tokens = count_tokens(bot_response)
             print("bot_response_tokens: ", bot_response_tokens)
             # 修正: system_messageとself.message_history[user_key]のトークン数をそれぞれ数え、それらを合計する
-            total_tokens_used = count_tokens(system_message["content"]) + sum(count_tokens(m["content"])
-                                                                              for m in self.message_history[user_key])
+            total_tokens_used = count_tokens(system_message["content"]) + sum(count_tokens(m["content"]) for m in self.message_history[user_key])
             # 今回のリクエスト時に消費したトークン数を表示
             print("今回のリクエストで消費した合計トークン数: ", total_tokens_used)
             # メッセージ履歴にボットの返答を追加

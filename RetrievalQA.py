0964c59cc07e254a3da28c3334509d175b24aadb
syusuke9999 <6879863+syusuke9999@users.noreply.github.com>
2023-08-13 16:29:08 +0900

    perf: Modified class reference
    Modified the derived classes 'GetAnswerFromFaiss' and
    'GetResponseWithFaiss' to access 'message_histories' in the base class
    'MyBot'.

--- RetrievalQA.py
+++ RetrievalQA.py
@@ -29,44 +29,11 @@ class RetrievalQAFromFaiss:
             docsearch = FAISS.load_local("./faiss_index", embeddings)
             compression_retriever = ContextualCompressionRetriever(base_compressor=embeddings_filter,
                                                                    base_retriever=docsearch.as_retriever())
-
-            # 現在の日付と時刻を取得します（日本時間）。
-            now = datetime.now(pytz.timezone('Asia/Tokyo'))
-            # 年、月、日を取得します。
-            year = now.year
-            month = now.month
-            day = now.day
-            # 直近のメッセージを取得
-            recent_messages = self.message_histories[user_key][-10:]  # 5往復分なので、最後の10メッセージ
-            # 対話形式に変換
-            dialogue_format = ""
-            for msg in recent_messages:
-                role = "User" if msg['role'] == 'user' else "Assistant"
-                dialogue_format += f"{role}: {msg['content']}\n"
-            print("dialogue_format: ", dialogue_format)
-            custom_prompt = (f"Previous Conversation: {dialogue_format}\n"
-                             f"Today is the year {year}, the month is {month} and the date {day}."
-                             f"The current time is {now}."
-                             "Use the following pieces of context to answer the question at the end. If you don't "
-                             "know the answer,"
-                             " just say 「分かりません」, don't try to make up an answer. "
-                             "日本人として日本語を使って質問に答えて下さい。"
-                             " \n"
-                             "Context:{context}"
-                             " \n"
-                             "Question: {question}"
-                             "Helpful Answer:")
-            stuff_prompt = PromptTemplate(
-                template=custom_prompt,
-                input_variables=["context", "question"]
-            )
-            chain_type_kwargs = {"prompt": stuff_prompt}
             refine_qa = RetrievalQA.from_chain_type(
                 llm=llm,
                 chain_type="refine",
                 retriever=compression_retriever,
-                verbose=False,
-                chain_type_kwargs=chain_type_kwargs  # ここで変数stuff_promptを直接渡す
+                verbose=True,
             )
             # return_source_documentsプロパティをTrueにセット
             refine_qa.return_source_documents = True

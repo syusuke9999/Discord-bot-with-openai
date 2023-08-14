e9a1144b1ee31f6038afdefec0072bc37104d353
syusuke9999 <6879863+syusuke9999@users.noreply.github.com>
2023-08-15 03:03:59 +0900

    Change Retrieval QA response from stuff to refine.

--- RetrievalQA.py
+++ RetrievalQA.py
@@ -1,3 +1,4 @@
+from langchain.chains.summarize import load_summarize_chain
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.llms.loading import load_llm
 from langchain.chains import RetrievalQA
@@ -5,6 +6,7 @@ from langchain.vectorstores import FAISS
 from langchain.retrievers import ContextualCompressionRetriever
 from langchain.retrievers.document_compressors import EmbeddingsFilter
 from langchain.prompts import PromptTemplate
+from langchain.output_parsers import RegexParser  # New import for the output parser
 import os
 import asyncio
 from datetime import datetime
@@ -20,77 +22,37 @@ class RetrievalQAFromFaiss:
     async def GetAnswerFromFaiss(self, input_txt):
         self.input_txt = input_txt
         llm = load_llm("my_llm.json")
+        if "パークの効果" in input_txt:
+            input_txt = input_txt.replace("パークの効果", "パークの性能と効果")
         embeddings = OpenAIEmbeddings()
-        embeddings_filter = EmbeddingsFilter(embeddings=embeddings, top_k=3)
+        embeddings_filter = EmbeddingsFilter(embeddings=embeddings)
         source_url = ""
         if os.path.exists("./faiss_index"):
             docsearch = FAISS.load_local("./faiss_index", embeddings)
             compression_retriever = ContextualCompressionRetriever(base_compressor=embeddings_filter,
                                                                    base_retriever=docsearch.as_retriever())
-
-            # 現在の日付と時刻を取得します（日本時間）。
-            now = datetime.now(pytz.timezone('Asia/Tokyo'))
-            # 年、月、日を取得します。
-            year = now.year
-            month = now.month
-            day = now.day
-            custom_prompt = (f"Today is the year {year}, the month is {month} and the date {day}."
-                             f"The current time is {now}."
-                             "Use the following pieces of context to answer the question at the end. If you don't "
-                             "know the answer,"
-                             " just say 「分かりません」, don't try to make up an answer. "
-                             "Please use Japanese only. Don't use English."
-                             " \n"
-                             "Context:{context}"
-                             " \n"
-                             "Question: {question}"
-                             "Helpful Answer:")
-            stuff_prompt = PromptTemplate(
-                template=custom_prompt,
-                input_variables=["context", "question"]
-            )
-            chain_type_kwargs = {"prompt": stuff_prompt}
-            stuff_qa = RetrievalQA.from_chain_type(
+            # Change chain_type from "stuff" to "refine"
+            qa = RetrievalQA.from_chain_type(
                 llm=llm,
-                chain_type="stuff",
+                chain_type="refine",
                 retriever=compression_retriever,
-                verbose=True,
-                chain_type_kwargs=chain_type_kwargs  # ここで変数stuff_promptを直接渡す
             )
             # return_source_documentsプロパティをTrueにセット
-            stuff_qa.return_source_documents = True
+            qa.return_source_documents = True
             # applyメソッドを使用してレスポンスを取得
             loop = asyncio.get_event_loop()
             print(f"Input dict before apply: {input_txt}")
-            response = await loop.run_in_executor(None, lambda: stuff_qa.apply([input_txt]))
+            response = await loop.run_in_executor(None, lambda: qa.apply([self.input_txt]))
             # responseオブジェクトからanswerとsource_urlを抽出
             try:
-                stuff_answer = response[0]["result"]
+                answer = response[0]["result"]
             except (TypeError, KeyError, IndexError):
-                stuff_answer = "APIからのレスポンスに問題があります。開発者にお問い合わせください。"
-                print(f"stuff_answer: {stuff_answer}")
-                return stuff_answer, source_url, input_txt
+                answer = "APIからのレスポンスに問題があります。開発者にお問い合わせください。"
             try:
                 source_url = response[0]["source_documents"][0].metadata["source"]
-                print(f"source_url: {source_url}")
             except (TypeError, KeyError, IndexError):
                 source_url = None
-            refine_qa = stuff_qa.from_chain_type(
-                chain_type="refine",
-                llm=llm,
-                retriever=compression_retriever,
-                verbose=True,
-            )
-            # applyメソッドを使用してレスポンスを取得
-            loop = asyncio.get_event_loop()
-            print(f"Input stuff_answer: {stuff_answer}")
-            response = await loop.run_in_executor(None, lambda: refine_qa.apply([stuff_answer]))
-            # responseオブジェクトからanswerとsource_urlを抽出
-            try:
-                refined_answer = response[0]["result"]
-            except (TypeError, KeyError, IndexError):
-                refined_answer = "APIからのレスポンスに問題があります。開発者にお問い合わせください。"
-                print(f"refined_answer: {refined_answer}")
-                return refined_answer, source_url, input_txt
-
-            return refined_answer, source_url, input_txt
+            return answer, source_url, self.input_txt
+        else:
+            answer = "申し訳ありません。データベースに不具合が生じているようです。開発者にお問い合わせください。"
+            return answer, source_url, self.input_txt

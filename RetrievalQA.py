ed9e57ff501a60bb8ba1cae336dcd8b4d3d689d5
syusuke9999 <6879863+syusuke9999@users.noreply.github.com>
2023-08-15 07:09:05 +0900

    Update index.

--- RetrievalQA.py
+++ RetrievalQA.py
@@ -31,44 +31,20 @@ class RetrievalQAFromFaiss:
         source_url = ""
         if os.path.exists("./faiss_index"):
             docsearch = FAISS.load_local("./faiss_index", embeddings)
-            search_kwargs = {"k": 6, "return_metadata": True}
+            search_kwargs = {"k": 4, "return_metadata": True}
             compression_retriever = ContextualCompressionRetriever(base_compressor=embeddings_filter,
                                                                    base_retriever=docsearch.as_retriever(
-                                                                       search_type="mmr",
-                                                                       search_kwargs=search_kwargs))
-
-            # 現在の日付と時刻を取得します（日本時間）。
-            now = datetime.now(pytz.timezone('Asia/Tokyo'))
-            # 年、月、日を取得します。
-            year = now.year
-            month = now.month
-            day = now.day
-            custom_prompt = (f"Today is the year {year}, the month is {month} and the date {day}."
-                             f"The current time is {now}."
-                             "Use the following pieces of context to answer the question at the end. If you don't "
-                             "know the answer,"
-                             " just say 「分かりません」, don't try to make up an answer. Answer the question"
-                             " as if you were a native Japanese speaker."
-                             " \n"
-                             "Context:{context}\n"
-                             " \n"
-                             "Question: {question}\n"
-                             "Helpful Answer:")
-            stuff_prompt = PromptTemplate(
-                template=custom_prompt,
-                input_variables=["context", "question"]
-            )
-            chain_type_kwargs = {"prompt": stuff_prompt}
-            stuff_qa = RetrievalQA.from_chain_type(
+                                                                    search_kwargs=search_kwargs))
+            refine_qa = RetrievalQA.from_chain_type(
                 llm=llm,
-                chain_type="stuff",
+                chain_type="refine",
                 retriever=compression_retriever,
-                chain_type_kwargs=chain_type_kwargs  # ここで変数stuff_promptを直接渡す
+                # chain_type_kwargs=chain_type_kwargs  # ここで変数stuff_promptを直接渡す
             )
             # return_source_documentsプロパティをTrueにセット
-            stuff_qa.return_source_documents = True
+            refine_qa.return_source_documents = True
             loop = asyncio.get_event_loop()
-            response = await loop.run_in_executor(None, lambda: stuff_qa.apply([input_txt]))
+            response = await loop.run_in_executor(None, lambda: refine_qa.apply([input_txt]))
             # responseオブジェクトからanswerとsource_urlを抽出
             try:
                 answer = response[0]["result"]
